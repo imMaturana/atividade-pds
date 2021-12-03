1d60592a706c76b6e55ef1f71638b64bb3a22ce3
maturanei <maturana.te@gmail.com>
2021-12-03 01:06:16 -0400

    .netcore-6.0 => .netcore-5.0

---
+++ Views/Login.axaml.cs
@@ -0,0 +1,22 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ds_atividade.Views
+{
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
